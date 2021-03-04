f628b8a42f11878b2cf8d497a8f4840ddb836627
Russell Brabers <brabers.russell@gmail.com>
2021-03-04 14:02:42 -0500

    Player Movement
    - Player can walk and run
    - Camera follows the player

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // Components
     private Animator anim;
+    private Rigidbody rigidBody;
+
+    private Vector2 inputVector = Vector2.zero;
+
+    //Animator Hashes
+    public readonly int MovementXHash = Animator.StringToHash("MovementX");
+    public readonly int MovementYHash = Animator.StringToHash("MovementY");
+    public readonly int IsRunningHash = Animator.StringToHash("isRunning");
 
     // Start is called before the first frame update
     void Start()
@@ -13,5 +22,15 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    public void OnMovement(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+        anim.SetFloat(MovementXHash, inputVector.x);
+        anim.SetFloat(MovementYHash, inputVector.y);
+    }
 
+    public void OnRun(InputValue value)
+    {
+        anim.SetBool(IsRunningHash, value.isPressed);
+    }
 }
